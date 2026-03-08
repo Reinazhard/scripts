d876f4e3fa1e08af608346c71e70894edfaead79
M. "Harumajati" Alfarozi <reinazhard@gmail.com>
2026-03-08 20:40:15 +0700

    LICENSE: Update
    Signed-off-by: M. "Harumajati" Alfarozi <reinazhard@gmail.com>

--- raviole/add_subtree.py
+++ raviole/add_subtree.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2025 M. "Harumajati" Alfarozi
+Copyright (c) 2026 M. "Harumajati" Alfarozi
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
